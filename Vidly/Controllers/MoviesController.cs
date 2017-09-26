c88ae2f45b6e5030a746f29e8b02229f83c6350a
gitter007 <erkashine@yahoo.com>
2017-09-26 10:24:42 -0500

    Enabled migrations

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -7,6 +7,23 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id=1, Name="Shrek"},
+                new Movie {Id=2, Name="Avatar"}
+            };
+
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
